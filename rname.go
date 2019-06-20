2191be398d818729f8a244496256951be6eaf85f
serpial <phutchison8@gmail.com>
2019-06-20 11:03:38 +0100

    Restarted project. This validates the arguments that the user has inputed. It validates that the file we are to change already exists and that the new file does not and the new file only uses certain characters

--- rname.go
+++ rname.go
@@ -7,81 +7,108 @@ import (
 )
 
 func main() {
-	arguments := os.Args // type []string
+	arguments := os.Args
 
-	switch len(arguments){
-	case 2:
-		if arguments[1] == "-h" || arguments[1] == "--help" {
-			printHelp()
-			return
-		}
-	case 3:
-		if isFile(arguments) == 1 {
-			if !strings.Contains(arguments[2], "/") {
-				doRename(arguments)
-			} else {
-				fmt.Println("Invalid Argument: This program assumes" +
-					" the path.")
-			}
-		} else {
-			basicError()
-		}
-	case 4:
-		if isFile(arguments) == 2 {
-			if arguments[1] == "-f" {
-				fmt.Println ("Some folder-y stuff")
-			}
-		} else {
-			basicError()
-		}
-	default:
-		basicError()
+	if checkInput(arguments) == 0 {
+		fmt.Println("Success")
 	}
 }
 
-func getLocation(arguments []string) string {
-	if strings.Contains(arguments[len(arguments)-2],"/") {
-		str := arguments[len(arguments)-2]
-		s := strings.Split(str, "/")
-		return str[:len(str) - len(s[len(s)-1])]
-	} else {
-		return ""
-	}
-}
 
-func printHelp() {
-	fmt.Println("Usage:\n\t" +
-		"rname [path to file] [new name]\n\n" +
-		"Arguments:\n\t" +
-		"-f\t\tchange the name of a folder\n\t" +
-		"-h, --help\tshows usage and arguments\n\t")
-}
-
-func isFile (arguments []string) byte {
-	fi, err := os.Stat(getLocation(arguments)+arguments[len(arguments)-2])
-    if err != nil {
-        fmt.Println(err)
-        return 0
-    }
-	
-    switch mode := fi.Mode(); {
-	case mode.IsRegular():
+/*
+* Returns 0 if the input is fine and we are good to do a rename
+* Returns 1 if there is an issue with the input
+* Returns 2 if the input was fine and we just returned the usage screen
+*/
+func checkInput(arguments []string) int {
+	if len(arguments)<2 || len(arguments)>3{
+		fmt.Println("rname: There was an issue with the" +
+			" number of arguments you entered")
 		return 1
-    case mode.IsDir():
+	} else if len(arguments)==2 {
+		// This is where other arguements can be selected like '--help'
+		if arguments[1]=="--help" || arguments[1]=="-h" {
+			printHelpAndUsage()
+		} else {
+			fmt.Println("Not an option")
+			fmt.Println("See -h or --help for information")
+		}
 		return 2
-    }
+	}
+
+	// Check to see if the first argument is a file that exists
+	valid := checkIfValidFile(arguments[1])
+	if valid>0 {
+		if valid==1 {
+			fmt.Println("Error: This program cannot be used with directories : "+
+				arguments[1])			
+		} else {
+			fmt.Println("Error: No such file or directory : " + arguments[1])
+		}
+		return 1
+	}
+
+	// Now that we have dealt with the 1st argument, it would be a good idea
+	//    to sanitize the second argument
+	if sanitizeNewFileName(arguments)==1 { return 1	}
+
+	// Check if the new name already exists
+	valid = checkIfValidFile(arguments[2])
+	if valid<2 {
+		if valid==0 {
+			fmt.Println("Error: New file name already exists in this directory")
+		} else {
+			fmt.Println("Error: New file name is the same as a directory")
+		}
+		return 1
+	}
+	
 	return 0
 }
 
-func basicError() {
-	fmt.Println("Invalid Argument:" +
-		" Check Usage via \"rname -h\"")
+/*
+* Uses whitelisting to restrict the characters the user can use 
+*/
+func sanitizeNewFileName(arguments []string) int {
+	whiteList := "abcdefghijklmnopqrstuvwxyz" +
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890" +
+		",.@#[]{}¬`!£$%^()_-+= " 
+
+	if len(arguments[2])>255 {
+		fmt.Println("Error: New name is too large")
+	}
+
+	for _, el := range arguments[2] {
+		if !(strings.Contains(whiteList, string(el))) {
+			fmt.Println("Error: New name contains illicit character" +
+				" : \"" + string(el) + "\"")
+			return 1;
+		}
+	}
+	return 0
 }
 
-func doRename(arguments []string) {
-	newNameAndLoc := getLocation(arguments)+arguments[len(arguments)-1]
-	e := os.Rename(arguments[len(arguments)-2], newNameAndLoc)
-	if e != nil {
-		fmt.Println(e)
+/*
+* To be used to check if the file is a file and if it already exists
+* Returns 0 if the file exists
+* Returns 1 if the file is actually a directory
+* Returns 2 if the file does not exist
+*/
+func checkIfValidFile(fileString string) int {
+	file, err := os.Stat(fileString)
+	if err != nil {	return 2 }
+
+	if file.Mode().IsDir() {
+		return 1
 	}
+	return 0
+}
+
+func printHelpAndUsage() {
+	fmt.Println("Basic program that allowes the renaming" +
+		" of an individual file")
+	fmt.Println("Usage: rname <File Location> <New Name>" +
+		"\n\t e.g. rname ~/Documents/file fileTwo")
+	fmt.Println("\nOptions:\n\t--help or -h\tDisplay usage" +
+		" and options")
 }
